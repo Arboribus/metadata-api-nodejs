93ea43724b4f59b5abd93f3ca049e9305b1b6a79
Andres Junge <andres.junge@consensys.net>
2019-09-26 12:56:55 -0300

    images

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express()
   .set('view engine', 'ejs')
 
 // Static public files
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'images')))
 
 app.get('/', function(req, res) {
   res.send('Arboribus Metadata API Service');
--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-const HOST = 'https://birthstamps.herokuapp.com'
+const HOST = 'https://api.arboribus.network/images/'
 
 module.exports = {
   HOST
